84ceca62a080d4fa8a09ddbd8fd1db4595f39b33
jvferreira <jvferreira@jvferreira10.indra.es>
2020-02-12 12:03:34 -0300

    Teste GIit

---
+++ ExerciciosDeProgramcao/src/Ex1.java
@@ -0,0 +1,28 @@
+
+public class Ex1 {
+	
+	public static long toMilesPerHour (double kilometerPerHour){
+		if(kilometerPerHour < 0){
+			return -1;
+		}else {
+			
+			//Método Math.round retorna um long, desta forma podemos converter o double do parametro
+			long milesPerHour = Math.round(kilometerPerHour / 1.609);
+			return milesPerHour;
+		}
+	}
+
+	public static void printConversion (double kilometersPerHour){
+		if(kilometersPerHour < 0){
+			System.out.println("Invalid Value");
+		} else{
+			System.out.println(kilometersPerHour + " KM/H = " + toMilesPerHour(kilometersPerHour) + " MI/H" );
+		}
+	}
+	
+	public static void main(String[] args) {
+		printConversion(1.5);
+	}
+}
+
+
