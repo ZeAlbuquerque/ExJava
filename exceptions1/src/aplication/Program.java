c8f2d05371784cab307f731796c2d74df2773eec
José Vitor Ferreira de Albuquerque <50268572+ZeAlbuquerque@users.noreply.github.com>
2020-02-18 17:32:52 -0300

    Versão final que ta show

--- exceptions1/src/aplication/Program.java
+++ exceptions1/src/aplication/Program.java
@@ -1,18 +1,21 @@
 package aplication;
 
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
 import model.entities.Reservation;
+import model.exception.DomainException;
 
 public class Program {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) {
 		Scanner tec = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
+		try {
 		System.out.print("Room number: ");
 		int roomNumber = tec.nextInt();
 		System.out.print("Check-in date (DD/MM/YYYY)): ");
@@ -20,28 +23,29 @@ public class Program {
 		System.out.print("Check-out date (DD/MM/YYYY): ");
 		Date checkOut = sdf.parse(tec.next());
 
-		if (!checkOut.after(checkIn)) {
-			System.out.print("Error in reservation: check_out date must be after check-in date");
+		Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+		System.out.println("Reservation: " + reservation);
+		System.out.println("");
 
-		} else {
-			Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
-			System.out.println("Reservation: " + reservation);
-			System.out.println("");
+		System.out.println("Enter data to update the reservation");
+		System.out.print("Check-in date (DD/MM/YYYY)): ");
+		checkIn = sdf.parse(tec.next());
+		System.out.print("Check-out date (DD/MM/YYYY)): ");
+		checkOut = sdf.parse(tec.next());
 
-			System.out.println("Enter data to update the reservation");
-			System.out.print("Check-in date (DD/MM/YYYY)): ");
-			checkIn = sdf.parse(tec.next());
-			System.out.print("Check-out date (DD/MM/YYYY)): ");
-			checkOut = sdf.parse(tec.next());
-
-			String error = reservation.updateDates(checkIn, checkOut);
-			if (error != null) {
-				System.out.println("Error in reservation: " + error);
-			} else {
-				System.out.println("Reservation: " + reservation);
-			}
-
+		reservation.updateDates(checkIn, checkOut);
+		System.out.println("Reservation: " + reservation);
 		}
+		catch(ParseException e){
+			System.out.println("Invalid date format");
+		}
+		catch(DomainException e) {
+			System.out.println("Error in reservation: " + e.getMessage());
+		}
+		catch(RuntimeException e) {
+			System.out.println("Unexpected error");
+		}
+
 		tec.close();
 	}
 
--- exceptions1/src/model/entities/Reservation.java
+++ exceptions1/src/model/entities/Reservation.java
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exception.DomainException;
+
 public class Reservation {
 
 	private int roomNumber;
@@ -16,7 +18,10 @@ public class Reservation {
 
 	}
 
-	public Reservation(int roomNumber, Date checkIn, Date checkOut) {
+	public Reservation(int roomNumber, Date checkIn, Date checkOut) throws DomainException {
+		if(!checkOut.after(checkIn)) {
+	    	throw new DomainException( "check_out date must be after check-in date");
+		}
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
@@ -39,17 +44,17 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff,TimeUnit.MILLISECONDS);
 	}
 	
-	public String updateDates(Date checkIn, Date checkOut) {
+	public void updateDates(Date checkIn, Date checkOut) throws DomainException{
 		Date now = new Date();
 		if(checkIn.before(now) || checkOut.before(now)) {
-			return "Reservation dates for update must be future";
+			//Execeção usada quandos os argumentos passados são invalidos
+			throw new DomainException("Reservation dates for update must be future");
 		}
 	    if(!checkOut.after(checkIn)) {
-			return "check_out date must be after check-in date";
+	    	throw new DomainException( "check_out date must be after check-in date");
 		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
-		return null;
 	}
 
 	@Override
